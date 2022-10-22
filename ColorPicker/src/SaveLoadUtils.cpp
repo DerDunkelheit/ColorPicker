5f7c8f6df442754bd5bdfe8f27a82c043e00ba08
DerDunkelheit <bogdoti7557@gmail.com>
2022-10-23 01:50:00 +0200

    little cleanup

--- ColorPicker/src/SaveLoadUtils.cpp
+++ ColorPicker/src/SaveLoadUtils.cpp
@@ -16,25 +16,28 @@ namespace SaveLoadUtils
         assert(std::filesystem::exists(PICKED_COLORS_FILE_NAME));
 
         std::vector<Objects::Color> returnData;
-        if (std::filesystem::exists(PICKED_COLORS_FILE_NAME))
+        if (std::filesystem::exists(PICKED_COLORS_FILE_NAME) && !std::filesystem::is_empty(PICKED_COLORS_FILE_NAME))
         {
             std::ifstream pickedColorsFile(PICKED_COLORS_FILE_NAME);
 
             std::string line;
             while (std::getline (pickedColorsFile, line))
             {
-                const size_t pos = line.find(DELIMITER);
-                if (pos != std::string::npos)
+                if (!line.empty())
                 {
-                    Objects::Color color = (ColorsUtils::GetColorFromHex(line.substr(0, pos).c_str()));
-                    const std::string comment = line.substr(pos + 1, line.length() - pos + 1);
-                    color.comment = comment;
+                    const size_t pos = line.find(DELIMITER);
+                    if (pos != std::string::npos)
+                    {
+                        Objects::Color color = (ColorsUtils::GetColorFromHex(line.substr(0, pos).c_str()));
+                        const std::string comment = line.substr(pos + 1, line.length() - pos + 1);
+                        color.comment = comment;
 
-                    returnData.push_back(color);
-                }
-                else
-                {
-                    returnData.push_back(ColorsUtils::GetColorFromHex(line.c_str()));
+                        returnData.push_back(color);
+                    }
+                    else
+                    {
+                        returnData.push_back(ColorsUtils::GetColorFromHex(line.c_str()));
+                    }
                 }
             }
             
